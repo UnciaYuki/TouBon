bfb641e1ba855a52bae14181060c52f606d9aec8
UnciaYuki <kim.thomasbrondeau@hotmail.com>
2024-12-05 17:22:18 +0100

    test

--- script.js
+++ script.js
@@ -9,80 +9,119 @@ Etape 5 : Je créer les cartes remplies dynamiquement
 
 //on donne l'url du site à appeller
 fetch('data.json')
-.then((rep) =>
-    rep.json())
-.then(donnee => {
-    // Affiche les données
-    afficheLesInfos(donnee);
-    console.log (donnee)
+    .then((rep) =>
+        rep.json())
+    .then(donnee => {
+        // Affiche les données
+        afficheLesInfos(donnee);
+        console.log(donnee)
 
-    // donnee.produits est un tableau
-    // je le parcours : 
-    // pour chacun des produit du tableau je l'affiche :
-    donnee.produits.forEach(prod=>{
-          afficheUnProduit(prod)
-    })
-    // donnee.services est un tableau
-    // je le parcours : 
-    // pour chacun des services du tableau je l'affiche :
-    donnee.services.forEach(serv=>{
-        AfficheService(serv)
-    })
+        // donnee.produits est un tableau
+        // je le parcours : 
+        // pour chacun des produit du tableau je l'affiche :
+        donnee.produits.forEach(prod => {
+            afficheUnProduit(prod)
+        })
+        // donnee.services est un tableau
+        // je le parcours : 
+        // pour chacun des services du tableau je l'affiche :
+        donnee.services.forEach(serv => {
+            AfficheService(serv)
+        })
 
-    // donnee.temoignages est un tableau
-    // je le parcours : 
-    // pour chacun des temoignages du tableau je l'affiche :
-    donnee.temoignages.forEach(temoignages=>{
-        AfficheTemoignage(temoignages)
-    })
+        // donnee.temoignages est un tableau
+        // je le parcours : 
+        // pour chacun des temoignages du tableau je l'affiche :
+        donnee.temoignages.forEach(temoignages => {
+            AfficheTemoignage(temoignages)
+        })
 
-    // avantagesClients est un tableau
-    // je le parcours : 
-    // pour chacun des services du tableau je l'affiche :
-    donnee.avantagesClients.forEach(tag=>{
-        AfficheavantagesClients(tag)
-    })
-});
+        // avantagesClients est un tableau
+        // je le parcours : 
+        // pour chacun des services du tableau je l'affiche :
+        donnee.avantagesClients.forEach(tag => {
+            AfficheavantagesClients(tag)
+        })
+    });
 // Rôle : Afficher un produit dans la page HTML sous forme de carte
 // Paramètre : "recette",  un objet représentant un produit contenant les informations du produit
 // Retour : Cette fonction ne retourne rien 
 
 function afficheLesInfos(TouBon) {
-    console.log (TouBon.nomCommercial)
+    console.log(TouBon.nomCommercial)
     document.querySelector("h1").innerHTML = TouBon.nomCommercial
-    console.log (TouBon.phraseAccroche)
+    console.log(TouBon.phraseAccroche)
     document.querySelector("h2").innerHTML = TouBon.phraseAccroche
-    console.log (TouBon.texteAppelAction)
+    console.log(TouBon.texteAppelAction)
     document.querySelector("a").innerHTML = TouBon.texteAppelAction
 }
 
-function afficheUnProduit(produit){
+function afficheUnProduit(produit) {
     // Role: afficher un produit dans la div qui a l'id divProduit
     // parametre: un produit du jeu de donnée
     // retour : rien
-    console.log(produit.nom)
-    console.log(produit.description)
-    console.log(produit.imageurl)
-}
-    // Role: afficher un service dans la div qui a l'id divService
-    // parametre: un service du jeu de donnée
-    // retour : rien
-function AfficheService(Serv){
-    console.log(Serv.nom)
-    console.log(Serv.description)
-}
-    // Role: afficher le témoignage dans la div qui a l'id divTemoignage
-    // parametre: témoignage du jeu de donnée
-    // retour : rien
+    let nom = produit.nom
+    let description = produit.description
+    let img = produit.imageurl
+    document.querySelector("#divProduit").innerHTML += 
 
-function AfficheTemoignage (temoignages){
-    console.log(temoignages.prenom)
-    console.log(temoignages.typeExperience)
-    console.log(temoignages.commentaire)
-    console.log(temoignages.note)
+    `
+    <div class="card">
+                        <img src="${img}" alt="">
+                        <div>
+                            <h3>${nom}</h3>
+                            <p>${description}</p>
+                        </div>
+    </div>
+    `
+
+}
+// Role: afficher un service dans la div qui a l'id divService
+// parametre: un service du jeu de donnée
+// retour : rien
+function AfficheService(Serv) {
+    let nom = Serv.nom
+    let description = Serv.description
+    let img = Serv.icon
+    document.querySelector("#divServ").innerHTML += 
+
+    `
+    <div class="flex spaceBetween center alignCenter margin card">
+        <img src="${img}" alt=""><i class=""></i>
+        <h4>${nom}</h4>
+        <p>${description}</p>
+    </div>
+ `
+}
+// Role: afficher le témoignage dans la div qui a l'id divTemoignage
+// parametre: témoignage du jeu de donnée
+// retour : rien
+
+function AfficheTemoignage(temoignages) {
+    let prenom = temoignages.prenom
+    let typeExperience = temoignages.typeExperience
+    let commentaire = temoignages.commentaire
+    let note = temoignages.note
+    document.querySelector("#divtemoignages").innerHTML +=
+`
+    <div class="cardT">
+                        <p>${prenom}</p>
+                        <p>★${note}</p>
+                        <p>${typeExperience}</p>
+                        <p>${commentaire}</p>
+                    </div>
+    `
 }
 
-function AfficheavantagesClients (tag){
-    console.log(tag.icon)
-    console.log(tag.avantage)
+function AfficheavantagesClients(tag) {
+    let icon = (tag.icon)
+    let avantage = (tag.avantage)
+
+document.querySelector("#divtag").innerHTML +=
+`
+<div class="tag">
+                        <img src="${icon}" alt="">
+                        <p>${avantage}</p>
+                    </div>
+`
 }
